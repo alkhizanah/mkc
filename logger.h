98a3ca3daa388b4957254f7cdac03739927066be
Hexogen <nazhim.alakkash@gmail.com>
2025-12-29 00:46:05 +0200

    exception handling and basic cli

--- logger.h
+++ logger.h
@@ -2,6 +2,7 @@
 #define LOGGER_H_
 #include <iostream>
 #include <string>
+#include "config.h"
 
 #define WHITE           "\033[37m"
 #define BLACK           "\033[30m"
@@ -10,15 +11,34 @@
 #define YELLOW          "\033[33m"
 #define RESET           "\033[0m"
 
-struct Logger {
-  void failLog(std::string message) {
-    std::cout << "\n[" << RED << "LOG" << "] " << RESET << message << "\n";
+
+static void debug(std::string message) {
+  std::cout << "\n[" << BLACK << "DEBUG" << RESET << "] " << message;
+}
+class Logger {
+private: 
+  Verbosity vb;
+public:
+  Logger(Verbosity verbosity) : vb(verbosity) {};
+
+  void successLog(std::string message) const {
+    std::cout << "\n[" << GREEN << "LOG" << RESET << "] " << message;
   }
-  void successLog(std::string message) {
-    std::cout << "\n[" << GREEN << "LOG" << "] " << RESET << message << "\n";
+  void failLog(std::string message) const {
+    std::cout << "\n[" << RED << "LOG" << RESET << "] " << message;
   }
-  void warningLog(std::string message) {
-    std::cout << "\n[" << YELLOW << "LOG" << "] " << RESET << message << "\n";
+  void failLog(std::string message, const char * exception) const {
+    std::cout << "\n[" << RED << "LOG" << RESET << "] " << message;
+    if (vb == Verbosity::verbose) {
+      if (exception) {
+        std::cout << "\n[" << RED << "EXCEPTION" << RESET << "] " << exception;
+      } else {
+        std::cout << "\n[" << RED << "EXCEPTION" << RESET << "] " << "unknown exception.";
+      }
+    }
+  }
+  void warningLog(std::string message) const {
+    std::cout << "\n[" << YELLOW << "LOG" << RESET << "] " << message;
   }
 };
 
--- main.cc
+++ main.cc
@@ -1,11 +1,14 @@
 #include <filesystem>
 #include <fstream>
 #include <string>
+#include <cstdlib>
 #include <unordered_map>
 #include <vector>
 #include "config.h"
 #include "logger.h"
 
+#define ENABLE_EXCEPTIONS in.exceptions(std::ifstream::failbit | std::ifstream::badbit)
+#define LOGGER const Logger& logger
 namespace fs = std::filesystem;
 
 
@@ -26,27 +29,41 @@ struct HeaderFile {
 std::unordered_map<std::string, SourceFile> sources;
 std::unordered_map<std::string, HeaderFile> headers;
 
-void scan(const fs::path &root) {
-  for (auto &p : fs::recursive_directory_iterator(root)) {
-    if (!p.is_regular_file())
-      continue;
+void scan(const fs::path &root, LOGGER) {
+  if (!fs::exists(root)) {
+    logger.failLog("directory does not exist: " + root.string(), " function: scan() failed.");
+    std::cout << std:: endl;
+    exit(EXIT_FAILURE);
+  }
 
-    auto ext = p.path().extension().string();
+  try {
+    fs::recursive_directory_iterator iter(root);
+    for (auto &p : iter) {
+      if (!p.is_regular_file())
+        continue;
 
-    if (ext == ".cpp" || ext == ".c") {
-      sources[p.path().string()] = {
-          p.path(),
-          fs::path("build/obj") / p.path().filename().replace_extension(".o")};
-    } else if (ext == ".h") {
-      headers[p.path().string()] = {p.path()};
+      auto ext = p.path().extension().string();
+
+      if (ext == ".cpp" || ext == ".c") {
+        sources[p.path().string()] = {
+            p.path(), fs::path("build/obj") /
+                          p.path().filename().replace_extension(".o")}; // TODO: add checks for if p.path has no filename somehow
+      } else if (ext == ".h") {
+        headers[p.path().string()] = {p.path()};
+      }
     }
+  } catch (const fs::filesystem_error &e) {
+    logger.failLog("error iterating over \"" + root.string() + "directory, please check for permission denial, symlink loops, etc.", e.what());
   }
 }
 
-void parse_includes(SourceFile &src) {
-  std::ifstream in(src.path);
+void parse_includes(SourceFile &src, LOGGER) {
+  std::ifstream in(src.path); ENABLE_EXCEPTIONS;
+  if (!fs::exists(src.path)) {
+    logger.failLog("file does not exist: " + src.path.string(), " function: parse_includes() failed.");
+  }
   std::string line;
-
+try {
   while (std::getline(in, line)) {
     if (line.find("#include \"") == 0) {
       auto start = line.find('"') + 1;
@@ -54,35 +71,54 @@ void parse_includes(SourceFile &src) {
       src.includes.emplace_back(line.substr(start, end - start));
     }
   }
+  } catch (const std::ios_base::failure &e) {
+    logger.failLog("failed to parse includes for \"" + src.path.string() + "\"", e.what());
+  }
+
 }
 
-uint64_t hash_file(const fs::path &p) {
-  std::ifstream in(p, std::ios::binary);
+uint64_t hash_file(const fs::path &p, LOGGER) {
+  std::ifstream in(p, std::ios::binary); ENABLE_EXCEPTIONS;
+  if (!fs::exists(p)) {
+    logger.failLog("file does not exist: " + p.string(), " function: hash_file() failed.");
+  }
   uint64_t hash = 1469598103934665603ULL;
   char c;
-
-  while (in.get(c)) {
-    hash ^= static_cast<unsigned char>(c);
-    hash *= 1099511628211ULL;
+  try {
+    while (in.get(c)) {
+      hash ^= static_cast<unsigned char>(c);
+      hash *= 1099511628211ULL;
+    }
+  } catch (const std::ios_base::failure &e) {
+    logger.failLog("failed to read" + p.string(), e.what()); // if something fails it could be HERE
   }
   return hash;
 }
 
 std::unordered_map<std::string, uint64_t> old_hashes;
 
-void load_cache() {
-  std::ifstream in("build/.buildcache");
+void load_cache(std::string cachePath, LOGGER) {
+  debug("number of cached hashes: " + std::to_string(old_hashes.size()));
+  if (!fs::exists(cachePath)) {
+    logger.failLog("file does not exist: " + cachePath, "function: load_cache() failed");
+    return;
+  }
+  std::ifstream in(cachePath); ENABLE_EXCEPTIONS;
   std::string path;
   uint64_t h;
 
-  while (in >> path >> h) {
-    old_hashes[path] = h;
+  try {
+    while (in >> path >> h) {
+      old_hashes[path] = h;
+    }
+  } catch (const std::ios_base::failure &e) {
+    logger.failLog("failed to read \"" + cachePath + "\"", e.what());
   }
 }
 
-void mark_dirty(const Config& conf) {
+void mark_dirty(const Config& conf, LOGGER) {
   for (auto &[_, src] : sources) {
-    src.hash = hash_file(src.path);
+    src.hash = hash_file(src.path, logger);
 
     if (!old_hashes.count(src.path.string())          // < if file doesn't exist in our set of hashed files (it wasn't there last time we built)
         || old_hashes[src.path.string()] != src.hash // <  or it does exist, but the hash doesn't match the new one (the contents changed)
@@ -102,11 +138,11 @@ void mark_dirty(const Config& conf) {
 
         HeaderFile hf;
         hf.path = inc;
-        hf.hash = hash_file(inc);
+        hf.hash = hash_file(inc, logger);
         it = headers.emplace(pathOfInclude, std::move(hf)).first;
       }
 
-      it->second.hash = hash_file(it->second.path);
+      it->second.hash = hash_file(it->second.path, logger);
 
       if (old_hashes[pathOfInclude] != it->second.hash) { // if the hash that we just computed isn't the same as the one in the cached hashes
         src.dirty = true;
@@ -117,18 +153,60 @@ void mark_dirty(const Config& conf) {
   }
 }
 
-int main() {
+void printHelp() {
+  std::cerr << R"(
+Usage: mkc [option]
+
+Options:
+  verbose          Enable verbose logging
+  clean            Rebuild all files
+  track-external   Rebuild files affected by external headers outside the working dir
+)" << std::endl;
+}
+
+int main(int argc, char *argv[]) {
   Config config;
-  Logger logger;
-  logger.successLog("this is a message that means we compiled correctly");
-  logger.failLog("this is a message that means we compiled incorrectly");
-  logger.warningLog("this is a warning, but it's not about the warning.. it's about.. sending a message");
-  scan("src");
-  load_cache();
+  if (argc == 2) {
+    if (std::string(argv[1]) == "verbose") {
+      config.log_verbosity = Verbosity::verbose;
+    } else if (std::string(argv[1]) == "clean") {
+      config.rebuild_all = true;
+    } else if (std::string(argv[1]) == "track-external") {
+      config.rebuild_all = true;
+    } else {
+      printHelp();
+      return 1;
+    }
+  } else if (argc > 2) {
+    printHelp();
+    return 1;
+  }
+
+  Logger logger(config.log_verbosity);
+
+  // // =============== TODO: this is to be removed later
+  // if (config.rebuild_all) {
+  //   scan("src", logger);
+  //   mark_dirty(config, logger);
+  //   load_cache("build/.cache", logger);
+  //
+  //   for (auto &[_, s] : sources)
+  //     parse_includes(s, logger);
+  //
+  //   mark_dirty(config, logger);
+  //   std::cout << std::endl;
+  //   return 0;
+  // }
+  // // ==========================================
+
+  scan("src", logger);
+  load_cache("build/.cache", logger);
 
   for (auto &[_, s] : sources)
-    parse_includes(s);
+    parse_includes(s, logger);
 
-  mark_dirty(config);
+  mark_dirty(config, logger);
+
+  std::cout << std::endl;
   return 0;
 }
